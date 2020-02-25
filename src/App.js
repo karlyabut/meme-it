33d4f94897120d7279825d9498af939c3cc801dc
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-02-24 20:36:39 -0500

    consumed API request and displays meme names

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import MemeTemplate from './components/MemeTemplate';
 function App() {
   const [memes, setMemes] = useState({});
+  const items = [];
+
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
       .then(res => res.json())
-      .then(res => setMemes(res));
-  });
+      .then(res => setMemes(res.data.memes));
+  }, []);
+  for (let i in memes) {
+    items.push(<MemeTemplate key={i} props={memes[i].name} />);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <span>{JSON.stringify(memes)}</span>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{items}</span>
+      {/* <button onClick={() => <MemeTemplate />}>whe</button> */}
     </div>
   );
 }
---
+++ src/components/MemeTemplate.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const MemeTemplate = ({ props }) => {
+  return (
+    <div>
+      <h5>{props}</h5>
+    </div>
+  );
+};
+
+export default MemeTemplate;
