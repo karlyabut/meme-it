9bbf8f9b4538a15881a068592467846b43cbe7d3
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-03-05 19:27:28 -0500

    added axios

--- src/components/MemeTemplate.js
+++ src/components/MemeTemplate.js
@@ -1,27 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import '../components/MemeTemplate.css';
+import React, { useState, useEffect } from "react";
+import "../components/MemeTemplate.css";
+import axios from "axios";
 
 const MemeTemplate = ({ id, name, source }) => {
-  async function getId() {
-    fetch(
-      'https://cors-anywhere.herokuapp.com/https://api.imgflip.com/caption_image',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          'template_id': '231',
-          'username': 'imgflip_hubot',
-          'password': 'imgflip_hubot',
-          'text0': 'sample',
-          'text1': 'sampletext'
-        })
-      }
-    )
+  const headers = { "Content-Type": "multipart/form-data" };
+  function getId() {
+    axios
+      .post(
+        "https://cors-anywhere.herokuapp.com/https://api.imgflip.com/caption_image",
+        {
+          template_id: "1232",
+          username: "imgflip_hubot",
+          password: "imgflip_hubot",
+          text0: "sample",
+          text1: "sampletext"
+        },
+        { headers: headers }
+      )
       .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log(data);
+        console.log(res);
       });
+    // fetch(
+    //   'https://cors-anywhere.herokuapp.com/https://api.imgflip.com/caption_image',
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     body: {
+    //       template_id: id,
+    //       username: 'imgflip_hubot',
+    //       password: 'imgflip_hubot',
+    //       text0: 'sample',
+    //       text1: 'sampletext'
+    //     }
+    //   }
+    // ).then(res => {
+    //   res.json();
+    //   console.log(res.body);
+    // });
   }
   return (
     <div className="main" onClick={getId}>
